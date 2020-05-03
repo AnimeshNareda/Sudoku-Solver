47c28610b0177689da4a9adea719b04f5d3ea284
AnimeshNareda <64348286+AnimeshNareda@users.noreply.github.com>
2020-05-03 12:17:44 +0530

    Update solve&print.py

--- solve&print.py
+++ solve&print.py
@@ -1,9 +1,5 @@
 def solve(bo):
-    """
-    Solves a sudoku board using backtracking
-    :param bo: 2d list of ints
-    :return: solution
-    """
+  
     find = find_empty(bo)
     if find:
         row, col = find
@@ -23,14 +19,7 @@ def solve(bo):
 
 
 def valid(bo, pos, num):
-    """
-    Returns if the attempted move is valid
-    :param bo: 2d list of ints
-    :param pos: (row, col)
-    :param num: int
-    :return: bool
-    """
-
+    
     # Check row
     for i in range(0, len(bo)):
         if bo[pos[0]][i] == num and pos[1] != i:
@@ -55,12 +44,7 @@ def valid(bo, pos, num):
 
 
 def find_empty(bo):
-    """
-    finds an empty space in the board
-    :param bo: partially complete board
-    :return: (int, int) row col
-    """
-
+   
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
@@ -70,11 +54,7 @@ def find_empty(bo):
 
 
 def print_board(bo):
-    """
-    prints the board
-    :param bo: 2d List of ints
-    :return: None
-    """
+   
     for i in range(len(bo)):
         if i % 3 == 0 and i != 0:
             print("- - - - - - - - - - - - - -")
